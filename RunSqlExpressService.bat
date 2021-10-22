32f8b25e616b3fcdf0109635839d87efaed22e90
Валерий <70258211+ValeryVerkhoturov@users.noreply.github.com>
2021-10-22 16:59:14 +0300

    add behavior for paused service

--- RunSqlExpressService.bat
+++ RunSqlExpressService.bat
@@ -1,14 +1,22 @@
 @echo off
 chcp 65001
 title Вкл./выкл. SQL Express Service
+cls
 
 for /F "tokens=3 delims=: " %%H in ('sc query "MSSQL$SQLEXPRESS" ^| findstr "        STATE"') do (
-    if /I "%%H" NEQ "RUNNING" (
-        net start "MSSQL$SQLEXPRESS"
+    :: Служба запущена
+    if /I "%%H" EQU "RUNNING" (
+        net stop MSSQL$SQLEXPRESS
         goto :break
     )
-    else if /I "%%H" NEQ "STOPPED" (
-        net  stop "MSSQL$SQLEXPRESS"
+    :: Служба приостановлена
+    if /I "%%H" EQU "PAUSED" (
+        net  continue MSSQL$SQLEXPRESS
+        goto :break
+    )
+    :: Служба остановлена
+    if /I "%%H" EQU "STOPPED" (
+        net  start MSSQL$SQLEXPRESS
         goto :break
     )
 )
